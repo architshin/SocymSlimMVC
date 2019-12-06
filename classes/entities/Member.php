f550a159d36ae8422a7b08d8a91295807a00ad41
Shinzo SAITO <architshin@websarva.com>
2019-12-06 14:55:34 +0900

    加工用ゲッタの追加。

--- classes/entities/Member.php
+++ classes/entities/Member.php
@@ -9,6 +9,47 @@ class Member
 	private $mbBirth;
 	private $mbType;
 
+	// Y年n月j日形式で加工された誕生日プロパティのゲッタ。
+	public function getMbBirthStr(): string
+	{
+		// 初期値として空文字列を用意。
+		$mbBirthStr = "";
+		// プロパティの$mbBirthがemptyでないならば…
+		if(!empty($this->mbBirth)) {
+			// $mbBirthを加工。
+			$mbBirthStr = date("Y年n月j日", strtotime($this->mbBirth));
+		}
+		// 結果をリターン。
+		return $mbBirthStr;
+	}
+
+	// 会員種類の整数値を文字列に変換した値のゲッタ。
+	public function getMbTypeStr(): string
+	{
+		// 初期値として空文字列を用意。
+		$mbTypeStr = "";
+		// プロパティ$mbTypeの値で処理を分岐。この場合switchが便利。
+		switch($this->mbType) {
+			case 1:
+				$mbTypeStr = "一般会員";
+				break;
+			case 2:
+				$mbTypeStr = "優良会員";
+				break;
+			case 3:
+				$mbBirthStr = "特別会員";
+				break;
+		}
+		// 結果をリターン。
+		return $mbTypeStr;
+	}
+
+	// 会員氏名を半角スペースで結合した文字列のゲッタ。
+	public function getMbNameFull(): string
+	{
+		return $this->mbNameLast." ".$this->mbNameFirst;
+	}
+
 	public function getId(): ?int
 	{
 		return $this->id;
