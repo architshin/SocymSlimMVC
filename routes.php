7f5dd7128ef781b0f0ae1c48b38a583fd7723f7e
Shinzo SAITO <architshin@websarva.com>
2019-12-06 22:23:17 +0900

    Chapter13のエンティティ節の内容。 (#1)
    * エンティティによる詳細画面表示まで完成。
    
    * 加工用ゲッタの追加。
    
    * リスト画面表示が完成。
    
    * 微修正。

--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 use SocymSlim\MVC\controllers\MemberController;
 
+$app->any("/showMemberList", MemberController::class.":showMemberList");
 $app->any("/goMemberAdd", MemberController::class.":goMemberAdd");
 $app->any("/memberAdd", MemberController::class.":memberAdd");
 $app->any("/showMemberDetail/{id}", MemberController::class.":showMemberDetail");
