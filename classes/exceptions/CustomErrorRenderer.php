1c0494111426af1e3ccceae9b74d4ec9aadf04f9
Shinzo SAITO <architshin@websarva.com>
2019-12-13 02:43:16 +0900

    デフォルトエラー画面との切り替え処理導入。

--- classes/exceptions/CustomErrorRenderer.php
+++ classes/exceptions/CustomErrorRenderer.php
@@ -5,6 +5,7 @@ use Throwable;
 use Psr\Container\ContainerInterface;
 use Slim\Interfaces\ErrorRendererInterface;
 use Slim\Views\Twig;
+use Slim\Error\Renderers\HtmlErrorRenderer;
 
 class CustomErrorRenderer implements ErrorRendererInterface
 {
@@ -19,12 +20,22 @@ class CustomErrorRenderer implements ErrorRendererInterface
 
 	public function __invoke(Throwable $exception, bool $displayErrorDetails): string
 	{
-		// Twigインスタンスをコンテナから取得。
-		$twig = $this->container->get("view");
-		// メッセージをテンプレート変数に格納。
-		$assign["errorMsg"] = "もう一度初めから操作してください。";
-		// Twigによりテンプレートから生成されたHTML文字列を取得。
-		$returnHtml = $twig->fetch("error.html", $assign);
+		// エラー詳細表示なら…
+		if($displayErrorDetails) {
+			// SlimのデフォルトHTMLエラーレンダラクラスインスタンスを生成。
+			$htmlErrorRenderer = new HtmlErrorRenderer();
+			// SlimのデフォルトHTMLエラーレンダラインスタンスを実行。
+			$returnHtml = $htmlErrorRenderer($exception, $displayErrorDetails);
+		}
+		// エラー詳細表示させないなら…
+		else {
+			// Twigインスタンスをコンテナから取得。
+			$twig = $this->container->get("view");
+			// メッセージをテンプレート変数に格納。
+			$assign["errorMsg"] = "もう一度初めから操作してください。";
+			// Twigによりテンプレートから生成されたHTML文字列を取得。
+			$returnHtml = $twig->fetch("error.html", $assign);
+		}
 		// HTML文字列をリターン。
 		return $returnHtml;
 	}
