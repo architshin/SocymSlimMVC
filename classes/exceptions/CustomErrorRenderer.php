887463ed98ab14d49c61f22583229020b99bfa5a
Shinzo SAITO <architshin@websarva.com>
2019-12-13 03:21:51 +0900

    404エラーへの対応。

--- classes/exceptions/CustomErrorRenderer.php
+++ classes/exceptions/CustomErrorRenderer.php
@@ -6,6 +6,7 @@ use Psr\Container\ContainerInterface;
 use Slim\Interfaces\ErrorRendererInterface;
 use Slim\Views\Twig;
 use Slim\Error\Renderers\HtmlErrorRenderer;
+use Slim\Exception\HttpNotFoundException;
 
 class CustomErrorRenderer implements ErrorRendererInterface
 {
@@ -31,10 +32,17 @@ class CustomErrorRenderer implements ErrorRendererInterface
 		else {
 			// Twigインスタンスをコンテナから取得。
 			$twig = $this->container->get("view");
-			// メッセージをテンプレート変数に格納。
-			$assign["errorMsg"] = "もう一度初めから操作してください。";
-			// Twigによりテンプレートから生成されたHTML文字列を取得。
-			$returnHtml = $twig->fetch("error.html", $assign);
+			// 発生した例外がHttpNotFoundExceptionなら…
+			if($exception instanceof HttpNotFoundException) {
+				// テンプレートとして404.htmlを利用。
+				$returnHtml = $twig->fetch("404.html");
+			}
+			else {
+				// メッセージをテンプレート変数に格納。
+				$assign["errorMsg"] = "もう一度初めから操作してください。";
+				// Twigによりテンプレートから生成されたHTML文字列を取得。
+				$returnHtml = $twig->fetch("error.html", $assign);
+			}
 		}
 		// HTML文字列をリターン。
 		return $returnHtml;
