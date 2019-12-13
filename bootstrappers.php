9c031bca01dd75b5d3a22f73a7daf755d425f610
Shinzo SAITO <architshin@websarva.com>
2019-12-14 01:20:10 +0900

    $displayErrorDetailsを環境変数から取得するように改造。

--- bootstrappers.php
+++ bootstrappers.php
@@ -1,7 +1,12 @@
 <?php
 use SocymSlim\MVC\exceptions\CustomErrorRenderer;
-
+// 環境変数DATABASE_URLを取得。
+$displayErrorDetails = getenv("DEV_MODE");
+// ルーティングミドルウェアを設定。
 $app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+//エラーミドルウェアを設定。
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, true, true);
+// デフォルトのエラーハンドラを取得。
 $errorHandler = $errorMiddleware->getDefaultErrorHandler();
+// HTMLのエラーレンダラとして独自クラスを設定。
 $errorHandler->registerErrorRenderer("text/html", CustomErrorRenderer::class);
